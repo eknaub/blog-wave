018d8e4e9e64810a0621017e82a90cd5fb78605a
knaub <eknaub404@gmail.com>
2025-07-08 17:54:52 +0200

    feat: stylings

--- frontend/src/app/dashboard/features/blog/components/blog-post-comment/blog-post-comment.ts
+++ frontend/src/app/dashboard/features/blog/components/blog-post-comment/blog-post-comment.ts
@@ -1,16 +1,18 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { BlogService } from '../../../../services/blog-service';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-blog-post-comment',
-  imports: [AsyncPipe, CommonModule],
+  imports: [AsyncPipe, CommonModule, MatExpansionModule],
   templateUrl: './blog-post-comment.html',
   styleUrl: './blog-post-comment.css',
 })
 export class BlogPostComment {
   blogService = inject(BlogService);
   postId = input.required<number>();
+  panelState = signal(false);
 
   comments = computed(() => {
     return this.blogService.getCommentsByPostId(this.postId());
--- frontend/src/app/dashboard/features/stats/components/stats-card/stats-card.ts
+++ frontend/src/app/dashboard/features/stats/components/stats-card/stats-card.ts
@@ -1,13 +1,17 @@
 import { Component, computed, inject } from '@angular/core';
 import { StatsService } from '../../../../services/stats-service';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { RouteNames } from '../../../../../shared/interfaces/routes';
 
 @Component({
   selector: 'app-stats-card',
-  imports: [],
+  imports: [MatIconModule, RouterLink],
   templateUrl: './stats-card.html',
   styleUrl: './stats-card.css',
 })
 export class StatsCard {
+  readonly RouteNames = RouteNames;
   statsService = inject(StatsService);
 
   totalPosts = this.statsService.posts;
